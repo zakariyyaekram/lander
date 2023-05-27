9d92d5db55831837bde0f209afa05deae048c671
theimpostor <sahirhoda@gmail.com>
2023-05-27 10:34:27 -0500

    shorten variable names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("game-area");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 // Set the canvas size to 400x400
 canvas.width = 400;
@@ -11,16 +11,15 @@ const mainEngineThrust = 0.03;
 
 const ship = {
   color: "black",
-  width: 8,
-  height: 22,
-  position: {
-    x: 200,
-    y: 200,
-  },
-  velocity: {
-    x: 0,
-    y: 0,
-  },
+  // height, width
+  w: 8,
+  h: 22,
+  // position
+  x: 200,
+  y: 200,
+  // velocity
+  dx: 0,
+  dy: 0,
   mainEngine: false,
   leftEngine: false,
   rightEngine: false,
@@ -28,100 +27,82 @@ const ship = {
 };
 
 function drawTriangle(a, b, c, fill) {
-  context.beginPath();
-  context.moveTo(a[0], a[1]);
-  context.lineTo(b[0], b[1]);
-  context.lineTo(c[0], c[1]);
-  context.lineTo(a[0], a[1]);
-  context.closePath();
-  context.fillStyle = fill;
-  context.fill();
+  ctx.beginPath();
+  ctx.moveTo(a[0], a[1]);
+  ctx.lineTo(b[0], b[1]);
+  ctx.lineTo(c[0], c[1]);
+  ctx.lineTo(a[0], a[1]);
+  ctx.closePath();
+  ctx.fillStyle = fill;
+  ctx.fill();
 }
 
 function drawSpaceship() {
-  context.save();
-  context.beginPath();
-  context.translate(ship.position.x, ship.position.y);
+  ctx.save();
+  ctx.beginPath();
+  ctx.translate(ship.x, ship.y);
   // context.rotate(ship.angle);
-  context.rect(
-    ship.width * -0.5,
-    ship.height * -0.5,
-    ship.width,
-    ship.height
-  );
-  context.fillStyle = ship.color;
-  context.fill();
-  context.closePath();
+  ctx.rect(ship.w * -0.5, ship.h * -0.5, ship.w, ship.h);
+  ctx.fillStyle = ship.color;
+  ctx.fill();
+  ctx.closePath();
 
   // Draw the flame if engine is on
   if (ship.mainEngine) {
     drawTriangle(
-      [ship.width * -0.5, ship.height * 0.5],
-      [ship.width * 0.5, ship.height * 0.5],
-      [0, ship.height * 0.5 + Math.random() * 10],
-      'orange'
-    )
+      [ship.w * -0.5, ship.h * 0.5],
+      [ship.w * 0.5, ship.h * 0.5],
+      [0, ship.h * 0.5 + Math.random() * 10],
+      "orange"
+    );
   }
   if (ship.rightEngine) {
     drawTriangle(
-      [ship.width * 0.5, ship.height * -0.25],
-      [ship.width * 0.5 + (Math.random() * 10), 0],
-      [ship.width * 0.5, ship.height * 0.25],
-      'orange'
-    )
+      [ship.w * 0.5, ship.h * -0.25],
+      [ship.w * 0.5 + Math.random() * 10, 0],
+      [ship.w * 0.5, ship.h * 0.25],
+      "orange"
+    );
   }
   if (ship.leftEngine) {
     drawTriangle(
-      [ship.width * -0.5, ship.height * -0.25],
-      [ship.width * -0.5 - (Math.random() * 10), 0],
-      [ship.width * -0.5, ship.height * 0.25],
-      'orange'
-    )
+      [ship.w * -0.5, ship.h * -0.25],
+      [ship.w * -0.5 - Math.random() * 10, 0],
+      [ship.w * -0.5, ship.h * 0.25],
+      "orange"
+    );
   }
-  context.restore();
+  ctx.restore();
 }
 
 function updateSpaceship() {
   // what forces acting on the ship?
   if (ship.rightEngine) {
-    ship.velocity.x -= sideEngineThrust;
+    ship.dx -= sideEngineThrust;
   }
   if (ship.leftEngine) {
-    ship.velocity.x += sideEngineThrust;
+    ship.dx += sideEngineThrust;
   }
   if (ship.mainEngine) {
-    ship.velocity.y -= mainEngineThrust;
+    ship.dy -= mainEngineThrust;
   }
 
   // gravity is always acting on the ship
-  ship.velocity.y += gravity
+  ship.dy += gravity;
 
   // after calculating velocity, update our position
-  ship.position.x += ship.velocity.x;
-  ship.position.y += ship.velocity.y;
-
-    
-
-  // mirror ship
-  // if (ship.position.x < 0)
-  //   ship.position.x += canvas.width
-  // if (ship.position.x >= canvas.width)
-  //   ship.position.x -= canvas.width
-  // if (ship.position.y < 0)
-  //   ship.position.y += canvas.height
-  // if (ship.position.y >= canvas.height)
-  //   ship.position.y -= canvas.height
+  ship.x += ship.dx;
+  ship.y += ship.dy;
 }
 
 function draw() {
   // Clear entire screen
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   updateSpaceship();
 
   // Begin drawing
   drawSpaceship();
-  /* other draw methods (to add later) */
 
   requestAnimationFrame(draw);
 }
@@ -142,9 +123,10 @@ function keyLetGo(event) {
       ship.mainEngine = false;
       break;
     default:
-      return
+      return;
   }
-  event.preventDefault()
+  // don't let arrow keys move screen around
+  event.preventDefault();
 }
 
 function keyPressed(event) {
@@ -163,13 +145,14 @@ function keyPressed(event) {
       ship.mainEngine = true;
       break;
     default:
-      return
+      return;
   }
-  event.preventDefault()
+  // don't let arrow keys move screen around
+  event.preventDefault();
 }
 
 function start() {
   document.addEventListener("keyup", keyLetGo);
   document.addEventListener("keydown", keyPressed);
-  draw();
+  requestAnimationFrame(draw);
 }
